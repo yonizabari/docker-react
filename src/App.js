9a9c950b9482fd788b85f4f690fbff189022190d
yoni.zabari <yoni.zabari@is.com>
2023-06-10 19:14:22 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi ok
+          BOOGA
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!!!!!!!!!!
         </a>
       </header>
     </div>
